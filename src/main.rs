170987e852e9a9712e803616975099b48329547a
Andrew Tolopko <atolopko@chanzuckerberg.com>
2023-12-27 21:18:31 -0500

    day 4, problem b

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // mod day2;
-mod day3;
+mod day4;
 fn main() {
-//    println!("Day 2a: {}", day2::day2a());
-//    println!("Day 2b: {}", day2::day2b());
-  println!("Day 3a: {}", day3::day3a());
-  println!("Day 3b: {}", day3::day3b());
+//  println!("Day 2a: {}", day2::day2a());
+//  println!("Day 2b: {}", day2::day2b());
+//  println!("Day 4a: {}", day3::day3a());
+//  println!("Day 3b: {}", day3::day3b());
+//  println!("Day 4a: {}", day4::day4a());
+  println!("Day 4b: {}", day4::day4b());
+
  }   
